b65f8d1f11fc1bfaee4edfab1c98826785671c0a
kimch0612 <kimch061279@gmail.com>
2020-05-25 13:14:53 +0900

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/univ_assignment.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/univ_assignment.dir/main.c.obj"
+  "libuniv_assignment.dll.a"
+  "univ_assignment.exe"
+  "univ_assignment.exe.manifest"
+  "univ_assignment.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/univ_assignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

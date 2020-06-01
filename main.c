ca5737bb8cf3f955e3015bcff216e454276404f8
kimch0612 <kimch061279@gmail.com>
2020-06-01 16:32:56 +0900

    .

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-   char code1 = 'A';
-   char code2 = 65;
-
-   printf("code1 = %c\n", code1);
-   printf("code2 = %c\n", code2);
+    char ch1 = "A", ch2='1';
+    printf("%c %c\n", ch1, ch2);
+    printf("%c %c\n", 65, 49);
+    printf("%d %d\n", ch1, ch2);
+    printf("%d %d\n", ch1+ch2, ch1+ch2);
 }

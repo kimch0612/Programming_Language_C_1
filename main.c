a1c91c63cd3ecfeb0b31fae746829f3e01d393e1
kimch0612 <kimch061279@gmail.com>
2020-06-01 16:23:17 +0900

    .

--- main.c
+++ main.c
@@ -2,14 +2,9 @@
 
 int main()
 {
-    int x;
-    printf("변수 x의 크기 : %d\n", sizeof(x));
-    printf("char형의 크기 : %d\n", sizeof(char));
-    printf("int형의 크기 : %d\n", sizeof(int));
-    printf("short형의 크기 : %d\n", sizeof(short));
-    printf("long형의 크기 : %d\n", sizeof(long));
-    printf("long long형의 크기 : %d\n", sizeof(long long));
-    printf("float형의 크기 : %d\n", sizeof(float));
-    printf("double형의 크기 : %d\n", sizeof(double));
-    return 0;
+   char code1 = 'A';
+   char code2 = 65;
+
+   printf("code1 = %c\n", code1);
+   printf("code2 = %c\n", code2);
 }

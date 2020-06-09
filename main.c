44f6af263c92252f48b5bf43b213eeab5f30e6ca
kimch0612 <kimch061279@gmail.com>
2020-06-09 11:02:46 +0900

    .

--- main.c
+++ main.c
@@ -3,22 +3,20 @@
 
 int main()
 {
-    int x, y, result;
-    printf("두개의 정수를 입력하시오: ");
-    scanf("%d %d", &x, &y);
+    double x, y, result;
+
+    printf("두개의 실수를 입력하시오 : ");
+    scanf("%lf %lf", &x, &y);
 
     result = x + y;
-    printf("%d + %d = %d\n", x, y, result);
+    printf("%f + %f = %f", x, y, result);
 
-    result = x - y; // 뺄셈
-    printf("%d - %d = %d\n", x, y, result);
+    result = x - y;
+    printf("%f - %f = %f", x, y, result);
 
-    result = x * y; // 곱셈
-    printf("%d * %d = %d\n", x, y, result);
+    result = x * y;
+    printf("%f * %f = %f", x, y, result);
 
-    result = x / y; // 나눗셈
-    printf("%d + %d = %d\n", x, y, result);
-
-    result = x % y; // 나머지
-    printf("%d %% %d = %d\n", x, y, result);
+    result = x / y;
+    printf("%f / %f = %f", x, y, result);
 }

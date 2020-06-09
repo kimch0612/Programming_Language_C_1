0752f0691fca0eeae21f20c0461254858f3e264f
kimch0612 <kimch061279@gmail.com>
2020-06-09 10:48:13 +0900

    .

--- main.c
+++ main.c
@@ -3,22 +3,22 @@
 
 int main()
 {
-   int x, y, result;
-   printf("두개의 정수를 입력하시오: ");
-   scanf("%d %d", &x, &y);
+    int x, y, result;
+    printf("두개의 정수를 입력하시오: ");
+    scanf("%d %d", &x, &y);
 
-   result = x + y;
-   printf("%d + %d = %d", x, y, result);
+    result = x + y;
+    printf("%d + %d = %d\n", x, y, result);
 
     result = x - y; // 뺄셈
-    printf("%d - %d = %d", x, y, result);
+    printf("%d - %d = %d\n", x, y, result);
 
     result = x * y; // 곱셈
-    printf("%d * %d = %d", x, y, result);
+    printf("%d * %d = %d\n", x, y, result);
 
     result = x / y; // 나눗셈
-    printf("%d + %d = %d", x, y, result);
+    printf("%d + %d = %d\n", x, y, result);
 
     result = x % y; // 나머지
-    printf("%d %% %d = %d", x, y, result);
+    printf("%d %% %d = %d\n", x, y, result);
 }

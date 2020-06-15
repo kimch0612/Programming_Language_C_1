d9efe6e4bb48c6dd1d7467a68fe39a80ab3a3a82
kimch0612 <kimch061279@gmail.com>
2020-06-15 13:46:25 +0900

    올림 변환과 내림 변환

--- main.c
+++ main.c
@@ -3,30 +3,14 @@
 
 int main(void)
 {
-    unsigned int num;
-    printf("십진수: ");
-    scanf("%u", &num);
+    char c;
+    int i;
+    float f;
 
-    unsigned int mask = 1 << 7;
-    printf("이진수: ");
-
-    ((num & mask) == 0) ? printf("0") : printf("1");
-    mask = mask >> 1;
-    ((num & mask) == 0) ? printf("0") : printf("1");
-    mask = mask >> 1;
-    ((num & mask) == 0) ? printf("0") : printf("1");
-    mask = mask >> 1;
-    ((num & mask) == 0) ? printf("0") : printf("1");
-    mask = mask >> 1;
-    ((num & mask) == 0) ? printf("0") : printf("1");
-    mask = mask >> 1;
-    ((num & mask) == 0) ? printf("0") : printf("1");
-    mask = mask >> 1;
-    ((num & mask) == 0) ? printf("0") : printf("1");
-    mask = mask >> 1;
-    ((num & mask) == 0) ? printf("0") : printf("1");
-    mask = mask >> 1;
-    printf("\n");
+    c = 10000;
+    i = 1.23456 + 10;
+    f = 10 + 20;
+    printf("c = %d, i = %d, f = %f", c, i, f);
 
     return 0;
 }

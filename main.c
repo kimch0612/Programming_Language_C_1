377088e9831c5a4198ca0f0ecafdda64f8d0c2e6
kimch0612 <kimch061279@gmail.com>
2020-06-15 12:40:47 +0900

    .

--- main.c
+++ main.c
@@ -1,18 +1,15 @@
 #pragma warning(disable: 4996)
 #include <stdio.h>
 
-int main()
+int main(void)
 {
     int x, y;
 
     printf("두개의 정수를 입력하시오: ");
     scanf("%d %d", &x, &y);
 
-    printf("x == y의 결과값 : %d\n", x == y);
-    printf("x != y의 결과값 : %d\n", x != y);
-    printf("x > y의 결과값 : %d\n", x > y);
-    printf("x < y의 결과값 : %d\n", x < y);
-    printf("x >= y의 결과값 : %d\n", x >= y);
-    printf("x <= y의 결과값 : %d\n", x <= y);
+    printf("%d && %d의 결과값 : %d\n", x, y, x && y);
+    printf("%d || %d의 결과값 : %d\n", x, y, x || y);
+    printf("!%d의 결과값: %d\n", x, !x);
     return 0;
 }

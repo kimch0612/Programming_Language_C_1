84984b683bac850dc39975aed16a0200e4032199
kimch0612 <kimch061279@gmail.com>
2020-06-15 13:05:51 +0900

    .

--- main.c
+++ main.c
@@ -5,11 +5,13 @@ int main(void)
 {
     int x, y;
 
-    printf("두개의 정수를 입력하시오: ");
-    scanf("%d %d", &x, &y);
+    printf("첫번째 수 = ");
+    scanf("%d", &x);
+    printf("두번째 수 = ");
+    scanf("%d", &y);
 
-    printf("%d && %d의 결과값 : %d\n", x, y, x && y);
-    printf("%d || %d의 결과값 : %d\n", x, y, x || y);
-    printf("!%d의 결과값: %d\n", x, !x);
+    printf("큰 수 = %d \n", (x > y) ? x:y);
+    printf("작은 수 = %d \n", (x < y) ? x:y);
+
     return 0;
 }

1e7b71b9ec8c73d0bcfbf888db2d9d5b4bed3e7b
kimch0612 <kimch061279@gmail.com>
2020-06-22 15:14:32 +0900

    goto문

--- main.c
+++ main.c
@@ -3,29 +3,13 @@
 
 int main(void)
 {
-    char op;
-    int x, y, result;
+    int i=1;
 
-    printf("수식을 입력하시오\n(예: 2 + 5)\n>>");
-    scanf("%d %c %d", &x, &op, &y);
-
-    switch(op) {
-        case '+':
-           result = x + y;
-           break;
-        case '-':
-            result = x - y;
-            break;
-        case '/':
-            result = x / y;
-            break;
-        case '*':
-            result = x * y;
-            break;
-        default:
-            printf("지원되지 않는 연산자입니다.");
-            break;
-    }
-    printf("%d %c %d = %d.\n", x, op, y, result);
+    loop:
+        printf("%d * %d = %d \n", 3, i, 3*i);
+        i++;
+        if(i==10) goto end;
+        goto loop;
+    end:
     return 0;
 }

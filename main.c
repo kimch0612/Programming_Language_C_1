1cc1346876d2dcb4935c06309f6e34a7db3efa2b
kimch0612 <kimch061279@gmail.com>
2020-06-22 14:23:29 +0900

    switch문

--- main.c
+++ main.c
@@ -3,20 +3,24 @@
 
 int main(void)
 {
-    int score;
+    int number;
 
-    printf("성적을 입력하시오: ");
-    scanf("%d", &score);
+    printf("점수를 입력하시오: ");
+    scanf("%d", &number);
 
-    if (score>=90)
-        printf("합격: 학점A\n");
-    else if (score>=80)
-        printf("합격: 학점B\n");
-    else if (score>=70)
-        printf("합격: 학점C\n");
-    else if (score>=60)
-        printf("합격: 학점D\n");
-    else
-        printf("불합격: 학점F\n");
-    return 0;
+    switch(number) {
+        case 0:
+            printf("없음\n");
+            break;
+        case 1:
+            printf("하나\n");
+            break;
+        case 2:
+            printf("둘\n");
+            break;
+        default:
+            printf("많음\n");
+            break;
+    }
+
 }
